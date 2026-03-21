d9bd2ff0c227086c451a6dda0ba09ce5b7a90737
SachinduHimash <himash1997@gmail.com>
2026-03-21 16:43:29 +0100

    Initial Commit

---
+++ WellLinkTests/WellLinkTests.swift
@@ -0,0 +1,17 @@
+//
+//  WellLinkTests.swift
+//  WellLinkTests
+//
+//  Created by Sachindu Himash Peiris on 21.03.2026.
+//
+
+import Testing
+@testable import WellLink
+
+struct WellLinkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
